77e7d0380247f66b44a6075380dc707f9bfbfe0b
bckstrb <rbccstrb@gmail.com>
2022-10-17 17:12:30 -0600

    wrote the CRUD functions for thoughtController.js and completed the routes for thoughtRoutes.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,2 +1,68 @@
-//refer to controllers in module
-//comment from act 21???
+const { Thought, Reaction } = require("../models");
+
+const thoughtController = {
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err = res.status(500).json(err)));
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body })
+      .then((thought) => {
+        if (!thought)
+          res.status(404).json({ error: "No thought exists with that ID" });
+        res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) => {
+        if (!thought)
+          res.status(404).json({ error: "No thought exists with that ID" });
+        return Reaction.deleteMany({ _id: { $in: thought.reactions } });
+      })
+      .then(() => {
+        res.json({ message: "Thought Deleted" });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.params.reactionId } },
+      { new: true }
+    )
+      .then((thought) => {
+        if (!thought)
+          res.status(404).json({ error: "No thought exists with that ID" });
+        res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: req.params.reactionId } },
+      { new: true }
+    )
+      .then((thought) => {
+        if (!thought)
+          res.status(404).json({ error: "No thought exists with that ID" });
+        res.json(thought);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = thoughtController; 
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,10 +3,15 @@ const {
   getThoughts,
   getSingleThought,
   createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
 } = require('../../controllers/thoughtController');
 
-// router.route('/').get(getThoughts).post(createThought);
+router.route('/').get(getThoughts).post(createThought);
 
-// router.route('/:thoughtId').get(getSingleThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions/: reactionId').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
